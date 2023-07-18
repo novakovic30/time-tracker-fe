f3e8e5a756b261ae7a1773e43e7ac5fb9987b059
MauriceDesalla <maurice.desalla@zaunergroup.com>
2023-07-18 11:32:00 +0200

    added simple table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatSortModule } from '@angular/material/sort'
+import { MatTableModule } from '@angular/material/table'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +24,13 @@ import { TaskModalComponent } from './components/home/task-modal/task-modal.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/user-tasks/user-tasks.component.ts
+++ src/app/components/home/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-tasks.component.css']
 })
 export class UserTasksComponent implements OnInit {
-
+  displayedColumns = ["ID", "Title", "Description", "Time spent", "Time for this week"];
+  dataSource!: MatTableDataSource<any>;
+  
   constructor() { }
 
   ngOnInit(): void {
+    const data = [
+      {ID: "1", Title: "yes", Description: "3", "Time spent": "3 hours", "Time for this week": "20 hours"}
+    ];
+
+    this.dataSource = new MatTableDataSource(data);
+  }
+
+  addRow(){
+    const newRow = { ID: '2', Title: 'no', Description: '5', 'Time spent': '5 hours', 'Time for this week': '25 hours' };
+    this.dataSource.data = [...this.dataSource.data, newRow];
   }
 
 }
