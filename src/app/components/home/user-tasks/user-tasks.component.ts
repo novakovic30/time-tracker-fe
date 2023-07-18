100e19c074e354056b572dbcd1a17282fe3735b0
MauriceDesalla <maurice.desalla@zaunergroup.com>
2023-07-18 15:42:24 +0200

    table design changed

--- src/app/components/home/user-tasks/user-tasks.component.ts
+++ src/app/components/home/user-tasks/user-tasks.component.ts
@@ -13,16 +13,21 @@ export class UserTasksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const data = [
-      {ID: "1", Title: "yes", Description: "3", "Time spent": "3 hours", "Time for this week": "20 hours"}
-    ];
-
-    this.dataSource = new MatTableDataSource(data);
+    this.updateTable();
   }
 
   addRow(){
     const newRow = { ID: '2', Title: 'no', Description: '5', 'Time spent': '5 hours', 'Time for this week': '25 hours' };
     this.dataSource.data = [...this.dataSource.data, newRow];
+  }
+
+  updateTable() {
+    //insert code which gets data from backend
+    const data = [
+      {ID: "1", Title: "yes", Description: "3", "Time spent": "3 hours", "Time for this week": "20 hours"}
+    ];
+
+    this.dataSource = new MatTableDataSource(data);
   }
 
 }
