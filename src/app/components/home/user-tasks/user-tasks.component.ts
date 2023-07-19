50a611131cc1ac978c1bb0857f42b252dd5b64c6
Robinson Pineyro Ortiz <robinson.pineyro-ortiz@zaunergroup.com>
2023-07-19 10:24:52 +0200

    new Design and Unique ID

--- src/app/components/home/user-tasks/user-tasks.component.ts
+++ src/app/components/home/user-tasks/user-tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table'
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,27 +7,40 @@ import { MatTableDataSource } from '@angular/material/table'
   styleUrls: ['./user-tasks.component.css']
 })
 export class UserTasksComponent implements OnInit {
-  displayedColumns = ["ID", "Title", "Description", "Time spent", "Time for this week"];
+  displayedColumns: string[] = ["ID", "Title", "Description", "Time spent", "Time for this week"];
   dataSource!: MatTableDataSource<any>;
-  
+  nextID: number = 2; // Initial value for ID of new rows
+
   constructor() { }
 
   ngOnInit(): void {
     this.updateTable();
   }
 
-  addRow(){
-    const newRow = { ID: '2', Title: 'no', Description: '5', 'Time spent': '5 hours', 'Time for this week': '25 hours' };
+  addRow() {
+    const newRow = {
+      ID: this.nextID.toString(),
+      Title: '<some Title>',
+      Description: '<short Description>',
+      'Time spent': '<type in hours>',
+      'Time for this week': '<type in hours>'
+    };
+    this.nextID++;
     this.dataSource.data = [...this.dataSource.data, newRow];
   }
 
   updateTable() {
-    //insert code which gets data from backend
+    // Insert code which gets data from backend
     const data = [
-      {ID: "1", Title: "yes", Description: "3", "Time spent": "3 hours", "Time for this week": "20 hours"}
+      {
+        ID: '1',
+        Title: '<some Title>',
+        Description: '<short Description>',
+        'Time spent': '<type in hours>',
+        'Time for this week': '<type in hours>'
+      },
     ];
 
     this.dataSource = new MatTableDataSource(data);
   }
-
 }
